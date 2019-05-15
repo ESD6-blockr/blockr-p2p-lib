3334a8efff0d621a64bf2051bbe325e603259bda
Hugo van de Vliert <hugovdv@outlook.com>
2019-05-15 11:30:43 +0200

    Removed commented debugging code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
 export { IPeer } from "./interfaces/iPeer";
-
-// Debugging:
-// import { Peer } from "./peer";
-//
-// // Example of an implementation of the first peer node
-// const peer: Peer = new Peer( "8081");
-//
-// // Example of an implementation of a peer connecting to another peer
-// const peer: Peer = new Peer( "8081", [`145.93.124.240`]);
