de84069b77121d3e634b8b8f24ff7b660368adb2
QuintAartsen <aartsenq@gmail.com>
2019-05-19 23:24:44 +0200

    refactored duplicate code an created a connection manager

--- src/concrete/peer.ts
+++ src/concrete/peer.ts
@@ -1,49 +1,36 @@
 import { logger } from "@blockr/blockr-logger";
 import { Guid } from "guid-typescript";
 
-import { MessageType } from "../enums";
+import { MessageType } from "../enums/messageType.enum";
 import { UnknownDestinationError } from "../exceptions/unknownDestinationError";
 import { IMessageListener } from "../interfaces/messageListener";
 import { IPeer, RESPONSE_TYPE } from "../interfaces/peer";
-import { Message } from "../models/message";
-import { Receiver } from "../receiver";
-import { RoutingTable } from "../routingTable";
-import { Sender } from "../sender";
+import { Message } from "../models/message.model";
+import { RoutingTable } from "../models/routingTable.model";
+import { ConnectionService } from "../services/connection.service";
+import { Receiver } from "../services/receiver.service";
+import { Sender } from "../services/sender.service";
 import { DateManipulator } from "../util/dateManipulator";
-import { rejects } from "assert";
-import { promises } from "fs";
 
 
-const MESSAGE_EXPIRATION_TIMER: number = 1;
-const MESSAGE_HISTORY_CLEANUP_TIMER: number = 60000; // One minute
 const DEFAULT_PORT: string = "8081";
+const THIS_IP: string = ""; // TODO: get the ip dynamic of the current machine
   
 /**
  * Handles the peer network.
  */
 export class Peer implements IMessageListener, IPeer {
-    private readonly routingTable: RoutingTable;
-    private readonly receiveHandlers: Map<string, (message: Message, senderGuid: string, response: RESPONSE_TYPE) => void>;
-    private sender?: Sender;
-    private receiver?: Receiver;
     private GUID?: string;
-    private readonly requestsMap: Map<string, (response: Message) => void>;
-
+    private readonly connectionService: ConnectionService;
+    
     constructor() {
-        this.receiveHandlers = new Map();
-        this.requestsMap = new Map();
-        this.routingTable = new RoutingTable();
-
+        this.connectionService = new ConnectionService();
+        this.createReceiverHandlers();
     }
 
     public init(port: string = DEFAULT_PORT, initialPeers?: string[]): Promise<void> {
         return new Promise(async (resolve) => {
-            this.createReceiverHandlers();
-            this.sender = new Sender(port);
-            this.receiver = new Receiver(this, port);
-
-            this.createRoutingTableCleanupTimer();
-
+            await this.connectionService.init(port);
             if (initialPeers) {
                 this.GUID = Guid.createEmpty().toString();
                 await this.checkInitialPeers(initialPeers);
@@ -56,135 +43,19 @@ export class Peer implements IMessageListener, IPeer {
     }
 
     /**
-     * Register custom receiver handlers.
-     *
-     * @param messageType - The messageType that the receiver handles
-     * @param implementation - The implementation of the receiver handler
-     */
-    public registerReceiveHandlerForMessageType(messageType: string, implementation: (message: Message, sender: string,
-                                                                                      response: RESPONSE_TYPE) => void): void {
-        this.receiveHandlers.set(messageType, implementation);
-    }
-
-    /**
-     * Send a message to the given destination.
-     *
-     * @param messageType - The message type
-     * @param destination - The destination GUID
-     * @param [body] - The message body
-     */
-    public sendMessage(messageType: string, destination: string, body?: string, responseImplementation?: RESPONSE_TYPE,
-                       correlationId?: string): Promise<void> {
-        return new Promise(async (resolve, reject) => {
-            logger.info(`leuke teskst destination: ${destination}`);
-            if (!this.sender || !this.GUID) {
-                reject();
-                return;
-            }
-            let destinationIp = destination;
-            if (messageType !== MessageType.JOIN) {
-                destinationIp = this.getIpFromRoutingTable(destination);
-            }
-            const message = new Message(messageType, this.GUID, body);
-            message.correlationId = correlationId;
-            if (responseImplementation) {
-                this.requestsMap.set(message.guid, responseImplementation);
-            }
-
-            logger.info(`ip: ${destinationIp} destination: ${destination}`);
-            await this.sender.sendMessage(message, destinationIp, destination);
-            resolve();
-        });
-    }
-
-    /**
-     * Send a broadcast to the network.
-     *
-     * @param messageType - The message type
-     * @param [body] - The message body
-     */
-    public sendBroadcast(messageType: string, body?: string, responseImplementation?: RESPONSE_TYPE): void {
-        if (!this.sender || !this.GUID) {
-            return;
-        }
-
-        for (const guid of this.routingTable.peers.keys()) {
-            const message = new Message(messageType, this.GUID, body);
-            const ip = this.routingTable.peers.get(guid);
-            if (ip) {
-                logger.info(messageType, "message");
-                this.sender.sendMessage(message, ip, guid);
-            }
-
-            if (responseImplementation) {
-                this.requestsMap.set(message.guid, responseImplementation);
-            }
-        }
-    }
-
-    /**
-     * Check of messageType of the given message has a known implementation, and executes the implementation.
-     *
-     * @param message - The incoming message
-     * @param senderGuid - The GUID of the sender
-     * @param senderIp - The IP of the sender
-     */
-    public onMessage(message: Message, senderGuid: string): void {
-        if (!this.sender) {
-            return;
-        }
-        
-        logger.info(`Message received from ${senderGuid}: ${message.type}`);
-        console.log(this.requestsMap);
-        console.log(message);
-
-
-        const implementation = this.receiveHandlers.get(message.type);
-        if (implementation && typeof implementation === "function") {
-
-            implementation(message, senderGuid, (responseMessage: Message) => {
-                this.sendMessage(responseMessage.type, responseMessage.originalSenderGuid, responseMessage.body, undefined, message.guid);
-            });
-
-            // Acknowledge this message
-            if (message.type !== MessageType.ACKNOWLEDGE) {
-                let destination;
-                if (message.body) {
-                    const body = JSON.parse(message.body);
-                    destination = body.ip;
-                }
-                if (!destination) {
-                    destination = this.getIpFromRoutingTable(senderGuid);
-                }
-                this.sender.sendAcknowledgeMessage(message, destination);
-            }
-        }
-    }
-
-    /**
-     * Leave the network.
-     */
-    public leave(): void {
-        this.sendBroadcast(MessageType.LEAVE);
-    }
-
-    /**
      * Create the default handlers that act on a received message, depending on the messageType.
      */
     private createReceiverHandlers(): void {
         // Handle acknowledge messages
-        this.registerReceiveHandlerForMessageType(MessageType.ACKNOWLEDGE, async (message: Message, senderGuid: string) => {
-            if (!this.sender) {
-                return;
-            }
-
+        this.connectionService.registerReceiveHandlerForMessageType(MessageType.ACKNOWLEDGE, async (message: Message, senderGuid: string) => {
             if (senderGuid && message.body) {
-                this.sender.removeSentMessage(message.body);
+                this.connectionService.removeSentMessage(message.body);
             }
         });
 
         // Handle join messages
-        this.registerReceiveHandlerForMessageType(MessageType.JOIN, async (message: Message, senderGuid: string, response: RESPONSE_TYPE) => {
+        this.connectionService.registerReceiveHandlerForMessageType(MessageType.JOIN, async (message: Message,
+                                                                                             senderGuid: string, response: RESPONSE_TYPE) => {
             // Check if node already has an id, if so do not proceed with join request
             if (message.originalSenderGuid === Guid.EMPTY && senderGuid) {
                 const newPeerId: string = Guid.create().toString();
@@ -193,37 +64,33 @@ export class Peer implements IMessageListener, IPeer {
                 }
                 const body = JSON.parse(message.body);
                 // Add the new peer to our registry
-                this.routingTable.addPeer(newPeerId, body.ip);
-                logger.info(`${newPeerId}==================================${body.ip}`);
-                console.log(this.routingTable);
+                this.connectionService.routingTable.addPeer(newPeerId, body.ip);
                 
                 // Send response
                 response(new Message(MessageType.JOIN_RESPONSE,
                     newPeerId,
-                    JSON.stringify({guid: newPeerId, ip: body.ip, routingTable: Array.from(this.routingTable.peers)})));
+                    JSON.stringify({guid: newPeerId, ip: body.ip, routingTable: Array.from(this.connectionService.routingTable.peers)})));
 
                 // Let other peers know about the newly joined peer
-                this.sendBroadcast(
-                    MessageType.NEW_PEER,
-                    JSON.stringify({guid: newPeerId, sender: body.ip}),
-                );
+                this.connectionService.sendBroadcast(new Message(MessageType.NEW_PEER,
+                    JSON.stringify({guid: newPeerId, sender: body.ip})));
             }
         });
 
         // Handle new peer messages
-        this.registerReceiveHandlerForMessageType(MessageType.NEW_PEER, async (message: Message, senderGuid: string) => {
+        this.connectionService.registerReceiveHandlerForMessageType(MessageType.NEW_PEER, async (message: Message, senderGuid: string) => {
             if (senderGuid && message.body) {
                 // Add the new peer to our registry
                 const body = JSON.parse(message.body);
-                this.routingTable.addPeer(body.guid, body.sender);
+                this.connectionService.routingTable.addPeer(body.guid, body.sender);
             }
         });
 
         // Handle leave messages
-        this.registerReceiveHandlerForMessageType(MessageType.LEAVE, async (message: Message, senderGuid: string) => {
+        this.connectionService.registerReceiveHandlerForMessageType(MessageType.LEAVE, async (message: Message, senderGuid: string) => {
             if (message && senderGuid) {
                 // Remove the new peer from our registry
-                this.routingTable.removePeer(message.originalSenderGuid);
+                this.connectionService.routingTable.removePeer(message.originalSenderGuid);
             }
         });
     }
@@ -232,62 +99,31 @@ export class Peer implements IMessageListener, IPeer {
      */
     private checkInitialPeers(peers: string[]): Promise<void> {
         return new Promise(async (resolve, reject) => {
-            if (!this.sender || !this.GUID) {
+            if (!this.GUID) {
                 reject();
                 return;
             }
             const promises = [];
             for (const peer of peers) {
                 // Check if peer is online and try to join
-                promises.push(this.sendMessage(MessageType.JOIN, peer, JSON.stringify({ip: "145.93.120.201"}), this.joinResponse));
+                const message = new Message(MessageType.JOIN, this.GUID, JSON.stringify({ip: THIS_IP}));
+                promises.push(this.connectionService.sendMessageByIp(message, peer, this.joinResponse));
             }
             await Promise.all(promises);
             resolve();
         });
     }
 
-    private joinResponse(message: Message) : Promise<void> {
-        console.log("joinResponse");
+    private joinResponse(message: Message): Promise<void> {
         return new Promise(async (resolve, reject) => {
             if (message.body && this.GUID === Guid.EMPTY && message.originalSenderGuid) {
                 const body = JSON.parse(message.body);
                 this.GUID = body.guid;
-                this.routingTable.addPeer(message.originalSenderGuid, body.ip);
-                this.routingTable.mergeRoutingTables(new Map(body.routingTable));
+                this.connectionService.routingTable.addPeer(message.originalSenderGuid, body.ip);
+                this.connectionService.routingTable.mergeRoutingTables(new Map(body.routingTable));
                 resolve();
             }
             reject();
         });
-    }
-
-
-    /**
-     * Create timer that removes peers that did not reply to a sent message.
-     *
-     * Used to remove offline peers
-     */
-    private createRoutingTableCleanupTimer() {
-        setInterval(() => {
-            if (!this.sender) {
-                return;
-            }
-            const minDate = DateManipulator.minusMinutes(new Date(), MESSAGE_EXPIRATION_TIMER);
-            for (const value of this.sender.getSentMessagesSendersSince(minDate)) {
-                this.routingTable.removePeer(value);
-                logger.info(`Peer removed from routing table: ${value}`);
-            }
-        }, MESSAGE_HISTORY_CLEANUP_TIMER);
-    }
-
-    private getIpFromRoutingTable(guid: string): string {
-        const destinationIp = this.routingTable.peers.get(guid);
-        logger.info(`${destinationIp}=======${guid}`);
-        console.log("nieuwe table");
-        console.log(this.routingTable);
-        if (!destinationIp) {
-            throw new UnknownDestinationError(`Unknown destination. Could not find an IP for: ${guid}`);
-        }
-
-        return destinationIp;
     }
 }
---
+++ src/services/connection.service.ts
@@ -0,0 +1,215 @@
+import { logger } from "@blockr/blockr-logger";
+import { Guid } from "guid-typescript";
+
+import { MessageType } from "../enums/messageType.enum";
+import { UnknownDestinationError } from "../exceptions/unknownDestinationError";
+import { IMessageListener } from "../interfaces/messageListener";
+import { RESPONSE_TYPE } from "../interfaces/peer";
+import { Message } from "../models/message.model";
+import { RoutingTable } from "../models/routingTable.model";
+import { Receiver } from "../services/receiver.service";
+import { Sender } from "../services/sender.service";
+import { DateManipulator } from "../util/dateManipulator";
+
+
+const MESSAGE_EXPIRATION_TIMER: number = 1;
+const MESSAGE_HISTORY_CLEANUP_TIMER: number = 60000; // One minute
+
+/**
+ * Handles the peer network.
+ */
+export class ConnectionService implements IMessageListener {
+    public readonly routingTable: RoutingTable;
+    private readonly receiveHandlers: Map<string, (message: Message, senderGuid: string, response: RESPONSE_TYPE) => void>;
+    private sender?: Sender;
+    private receiver?: Receiver;
+    private readonly requestsMap: Map<string, (response: Message) => void>;
+    private readonly sentMessages: Map<string, Message>;
+    private readonly sentMessageSenders: Map<string, string>;
+
+    
+    constructor() {
+        this.receiveHandlers = new Map();
+        this.requestsMap = new Map();
+        this.routingTable = new RoutingTable();
+        this.sentMessages = new Map<string, Message>();
+        this.sentMessageSenders = new Map<string, string>();
+    }
+
+    public init(port: string): Promise<void> {
+        return new Promise(async (resolve) => {
+            this.sender = new Sender(port);
+            this.receiver = new Receiver(this, port);
+            this.createRoutingTableCleanupTimer();
+            resolve();
+        });
+    }
+
+    /**
+     * Remove the given message from the sent messages history.
+     *
+     * @param messageGuid - The message hash of the message to remove
+     */
+    public removeSentMessage(messageGuid: string): void {
+        this.sentMessages.delete(messageGuid);
+        this.sentMessageSenders.delete(messageGuid);
+    }
+
+    /**
+     * Register custom receiver handlers.
+     *
+     * @param messageType - The messageType that the receiver handles
+     * @param implementation - The implementation of the receiver handler
+     */
+    public registerReceiveHandlerForMessageType(messageType: string, implementation: (message: Message, sender: string,
+                                                                                      response: RESPONSE_TYPE) => void): void {
+        this.receiveHandlers.set(messageType, implementation);
+    }
+
+    /**
+     * Send a message to the given destination.
+     *
+     * @param messageType - The message type
+     * @param destination - The destination GUID
+     * @param [body] - The message body
+     */
+    public sendMessage(message: Message, destinationGuid: string, responseImplementation?: RESPONSE_TYPE): Promise<void> {
+        if (destinationGuid) {
+            this.sentMessages.set(message.guid, message);
+            this.sentMessageSenders.set(message.guid, destinationGuid);
+        }
+
+        const destinationIp = this.getIpFromRoutingTable(destinationGuid);
+        return this.sendMessageByIp(message, destinationIp, responseImplementation);
+    }
+
+    /**
+     * Send a broadcast to the network.
+     *
+     * @param messageType - The message type
+     * @param [body] - The message body
+     */
+    public sendBroadcast(message: Message, responseImplementation?: RESPONSE_TYPE): Promise<void[]> {
+        const promises = [];
+        for (const guid of this.routingTable.peers.keys()) {
+            promises.push(this.sendMessage(message, guid, responseImplementation));
+        }
+        return Promise.all(promises);
+    }
+
+    /**
+     * Check of messageType of the given message has a known implementation, and executes the implementation.
+     *
+     * @param message - The incoming message
+     * @param senderGuid - The GUID of the sender
+     * @param senderIp - The IP of the sender
+     */
+    public onMessage(message: Message, senderGuid: string): void {
+        if (!this.sender) {
+            return;
+        }
+        logger.info(`Message received from ${senderGuid}: ${message.type}`);
+        
+        const implementation = this.receiveHandlers.get(message.type);
+
+        if (implementation && typeof implementation === "function") {
+
+            implementation(message, senderGuid, (responseMessage: Message) => {
+                this.sendMessage(responseMessage, responseMessage.originalSenderGuid);
+            });
+
+            // Acknowledge this message
+            if (message.type !== MessageType.ACKNOWLEDGE) {
+                let destination;
+                if (message.body) {
+                    const body = JSON.parse(message.body);
+                    destination = body.ip;
+                }
+                if (!destination) {
+                    destination = this.getIpFromRoutingTable(senderGuid);
+                }
+                this.sender.sendAcknowledgeMessage(message, destination);
+            }
+        }
+    }
+
+    /**
+     * Leave the network.
+     */
+    public leave(guid: string): void {
+        const message = new Message(MessageType.LEAVE, guid);
+        this.sendBroadcast(message);
+    }
+
+
+    public sendMessageByIp(message: Message, destinationIp: string, responseImplementation?: RESPONSE_TYPE): Promise<void> {
+        return new Promise(async (resolve, reject) => {
+            if (!this.sender || !this.GUID) {
+                reject();
+                return;
+            }
+            // const message = new Message(messageType, this.GUID, body);
+            // message.correlationId = correlationId;
+            
+            if (responseImplementation) {
+                this.requestsMap.set(message.guid, responseImplementation);
+            }
+            
+            await this.sender.sendMessage(message, destinationIp);
+            resolve();
+        });
+    }
+
+    /**
+     * Get guids from the senders of the sent messages after the given date.
+     * Deletes messages that are sent before the given date from the history.
+     *
+     * @param date - The date
+     *
+     * @return An array of the GUIDs of the senders
+     */
+    private getSentMessagesSendersSince(date: Date): string[] {
+        const guids: string[] = [];
+
+        for (const guid of this.sentMessages.keys()) {
+            const sentMessageSender = this.sentMessageSenders.get(guid);
+            const message = this.sentMessages.get(guid);
+
+            if (message && message.isOlderThan(date) && sentMessageSender) {
+                guids.push(sentMessageSender);
+
+                this.sentMessages.delete(guid);
+                this.sentMessageSenders.delete(guid);
+            }
+        }
+
+        return guids;
+    }
+
+
+    /**
+     * Create timer that removes peers that did not reply to a sent message.
+     *
+     * Used to remove offline peers
+     */
+    private createRoutingTableCleanupTimer() {
+        setInterval(() => {
+            if (!this.sender) {
+                return;
+            }
+            const minDate = DateManipulator.minusMinutes(new Date(), MESSAGE_EXPIRATION_TIMER);
+            for (const value of this.getSentMessagesSendersSince(minDate)) {
+                this.routingTable.removePeer(value);
+                logger.info(`Peer removed from routing table: ${value}`);
+            }
+        }, MESSAGE_HISTORY_CLEANUP_TIMER);
+    }
+
+    private getIpFromRoutingTable(guid: string): string {
+        const destinationIp = this.routingTable.peers.get(guid);
+        if (!destinationIp) {
+            throw new UnknownDestinationError(`Unknown destination. Could not find an IP for: ${guid}`);
+        }
+        return destinationIp;
+    }
+}
