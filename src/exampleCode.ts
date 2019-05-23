d2e73f2ff6f2809c3b0662d82a3fcb1cc6539460
QuintAartsen <aartsenq@gmail.com>
2019-05-23 11:55:37 +0200

    update example code

--- src/exampleCode.ts
+++ src/exampleCode.ts
@@ -1,5 +1,5 @@
 import { Peer } from "./concretes/peer";
-import { IPeer } from "./interfaces/peer";
+import { IPeer, RESPONSE_TYPE } from "./interfaces/peer";
 import { Message } from "./models";
 
 // Create the peer
@@ -8,10 +8,9 @@ const peer: IPeer = new Peer("examplePeer");
 peer.init("8081", ["145.93.101.81"]);
 
 // Add custom receive handler
-peer.registerReceiveHandlerForMessageType("testMessageType", async (message: Message, senderGuid: string) => {
+peer.registerReceiveHandlerForMessageType("testMessageType", async (message: Message, senderGuid: string, response: RESPONSE_TYPE) => {
     if (message && senderGuid) {
-        // Send the message back
-        peer.sendMessageAsync(message, senderGuid);
+        response(message);
     }
 });
 
@@ -20,8 +19,13 @@ const validatorGuid: string | undefined = peer.getPeerOfType("Validator");
 if (validatorGuid !== undefined) {
     // Create a message
     const message: Message = new Message("testMessageType", "testMessageType");
+
     // Send the message to the validator
-    peer.sendMessageAsync(message, validatorGuid);
+    peer.sendMessageAsync(message, validatorGuid, (responseMessage: Message) => {
+        Logger.info(responseMessage.correlationId);
+    });
+    peer.getPromiseForResponse(message);
+
     // Send the message to all peers in the network
     peer.sendBroadcastAsync(message);
 }
