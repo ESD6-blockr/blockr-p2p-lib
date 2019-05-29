63f05c6bcbccc6bafe3cff9eacb20865870fd075
QuintAartsen <aartsenq@gmail.com>
2019-05-29 11:51:48 +0200

    add IPFS type

--- src/enums/peerType.enum.ts
+++ src/enums/peerType.enum.ts
@@ -6,5 +6,6 @@ export enum PeerType {
     VALIDATOR = "validator",
     SMART_CONTRACT_ENGINE = "smart_contract_engine",
     WALLET = "wallet",
+    IPFS = "ipfs",
     INITIAL_PEER = "initial_peer",
 }
