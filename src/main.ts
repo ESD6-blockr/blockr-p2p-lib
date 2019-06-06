21800c9aa27af3f3da75408a0217e867f3abdf4e
QuintAartsen <aartsenq@gmail.com>
2019-06-06 10:21:44 +0200

    fix main

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,11 @@ import "reflect-metadata";
 import { Peer } from "./concretes/peer";
 import { PeerType } from "./enums/peerType.enum";
 import { IPeer } from "./interfaces/peer";
-import { Message } from "./models";
 
-const peer: IPeer = new Peer(PeerType.VALIDATOR);
+const peer: IPeer = new Peer(PeerType.INITIAL_PEER);
 
 async function start() {
-    await peer.init("8081", ["145.93.125.33"]);
-    const message = new Message("blockchain_and_states_request_response");
-    await peer.sendMessageToRandomPeerAsync(message, PeerType.VALIDATOR);
+    await peer.init();
 }
 start();
 
